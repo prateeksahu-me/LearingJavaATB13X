8be289578894fbdc80e470b3e056773b8ff12bd6
Prateek Sahu <prateeksahu0101@gmail.com>
2025-06-09 07:48:41 +0530

    new

---
+++ src/main/java/ex_01_Java_Basic/Lab002_Comments.java
@@ -0,0 +1,13 @@
+package ex_01_Java_Basic;
+
+public class Lab002_Comments {
+    public static void main(String[] args) {
+        //System.out.println("Hello World!");
+        //This is comment
+        System.out.println("Hello World!");
+        /*System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");*/
+
+    }
+}
